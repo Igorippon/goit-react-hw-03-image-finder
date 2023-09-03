7071a826e0486b99415a20d3fae3bb3fd6822080
Igorippon <botvunaigor@gmail.com>
2023-09-03 21:04:06 +0300

    Update api.js

--- src/api.js
+++ src/api.js
@@ -18,16 +18,4 @@ export const serviceSearch = async (searchQueryValue, page) => {
     return data;
 };
 
-// async function serviceSearch(searchQueryValue, page, per_page) {
-//     const params = new URLSearchParams({
-//         key: API_KEY,
-//         q: searchQueryValue,
-//         image_type: 'photo',
-//         orientation: 'orizontal',
-//         safesearch: 'true',
-//         per_page: '40',
-//         page: page,
-//     });
-//     const { data } = await axios.get(`/api/?${params}`);
-//     return data;
-// }
+
